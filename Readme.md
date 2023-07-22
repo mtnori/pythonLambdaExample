4513dfab199b9d07c4259ba985d2fa5c7c939da6
mtnori <moto44.noriyuki@gmail.com>
2023-07-22 10:40:38 +0900

    commit

--- Readme.md
+++ Readme.md
@@ -1,7 +1,17 @@
-# Commands for docker build and push
+# Python AWS Lambda with Docker image example
+
+## On AWS
 
 ```
 docker build -t python-lambda-sample:latest .
 docker tag python-lambda-sample:latest <account_id>.dkr.ecr.ap-northeast-1.amazonaws.com/python-lambda-sample:latest
-docker push <account_id>.dkr.ecr.ap-northeast-1.amazonaws.com/python-lambda-sample:latest
+docker push <account_id>.dkr.ecr.<region_name>.amazonaws.com/python-lambda-sample:latest
+```
+
+# On Local
+
+```
+docker build -t python-lambda-sample:latest .
+docker-compose -f minio/docker-compose.yml up -d
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
 ```
--- src/app.py
+++ src/app.py
@@ -4,16 +4,22 @@ from aws_lambda_typing import context as context_, events
 import boto3
 from pathlib import Path
 from mypy_boto3_s3 import S3Client
+from email.header import Header
 
 BUCKET_NAME = os.environ['BUCKET_NAME']
 OBJECT_KEY = os.environ['OBJECT_KEY']
+END_POINT_URL = os.getenv('END_POINT_URL')
 
 LOCAL_PATH = Path('/tmp/copy.png')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-s3: S3Client = boto3.client('s3')
+s3: S3Client = boto3.client(
+    's3',
+    endpoint_url=END_POINT_URL,
+    region_name='ap-northeast-1'
+)
 
 
 def handler(event: events.SQSEvent, context: context_.Context) -> None:
@@ -30,10 +36,18 @@ def handler(event: events.SQSEvent, context: context_.Context) -> None:
             file.write(i)
 
     # S3に書き出し
-    s3.upload_file(Filename=str(LOCAL_PATH), Bucket=BUCKET_NAME, Key='copy.png')
+    s3.upload_file(Filename=str(LOCAL_PATH),
+                   Bucket=BUCKET_NAME,
+                   Key='copy.png',
+                   ExtraArgs={
+                       'Metadata': {
+                           'filename': 'copy_image_file.png'
+                       }
+                   })
 
     # メタデータの取得方法
-    filename = s3_object['Metadata'].get('filename')
+    head_obj = s3.head_object(Bucket=BUCKET_NAME, Key='copy.png')
+    filename = head_obj['Metadata'].get('filename')
 
     # Optionalになるので、以下のように取得する
     if filename is not None:
